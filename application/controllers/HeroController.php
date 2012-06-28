3a4dd3cfc986a514ee76b8bd471b2290ba5f8825
Aaron Cox <aaron.cox@greymass.com>
2012-06-27 22:08:16 -0500

    No more anonymous hero creation.

--- application/controllers/HeroController.php
+++ application/controllers/HeroController.php
@@ -8,15 +8,20 @@
 class HeroController extends Epic_Controller_Action
 {
 	public function createAction() {
-		// Create a new hero
-		$hero = Epic_Mongo::newDoc('hero');
-		// Get Form for hero
-		$form = $this->view->form = $hero->getEditForm();
-		if($this->getRequest()->isPost()) {
-			$result = $form->process($this->getRequest()->getParams());
-			if($result) {
-				$i = Epic_Mongo::db('hero')->find($result['upserted']);
-				$this->_redirect("/h/".$i->id);				
+		$profile = Epic_Auth::getInstance()->getProfile();
+		if($profile) {		
+			// Create a new hero
+			$hero = Epic_Mongo::newDoc('hero');
+			// Get Form for hero
+			$form = $this->view->form = $hero->getEditForm();
+			if($this->getRequest()->isPost()) {
+				$result = $form->process($this->getRequest()->getParams());
+				if($result) {
+					$i = Epic_Mongo::db('hero')->find($result['upserted']);
+					$this->_redirect("/h/".$i->id);				
+				}
 			}
+		} else {
+			$this->_redirect("/user/heroes");
 		}
 	}} // END class HeroController extends Epic_Controller_Action
