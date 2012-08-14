c95122694eef094f136976feee958279b5674285
Aaron Cox <aaron.cox@greymass.com>
2012-08-14 17:51:23 -0500

    Log ajax requests

--- application/controllers/AjaxController.php
+++ application/controllers/AjaxController.php
@@ -8,6 +8,13 @@
 class AjaxController extends Epic_Controller_Action
 {
 	public function uploaditemAction() {
-		echo "<pre>"; var_dump($this->getRequest()->getParams()); exit;
+		$writer = new Zend_Log_Writer_Stream('/tmp/d3up-ajax');
+		$logger = new Zend_Log($writer);
+
+		$logger->info("-----------");
+		foreach($this->getRequest()->getParams() as $k => $v) {
+			$logger->info($k . ": " . $v);			
+		}
+		exit;
 	}
 } // END class AjaxController extends Epic_Controller_Action
