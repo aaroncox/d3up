0df52e67d91993942401044c83335a9cdb060ad8
Aaron Cox <aaron.cox@greymass.com>
2012-09-12 21:03:29 -0500

    Compare placeholder.

--- application/controllers/AjaxController.php
+++ application/controllers/AjaxController.php
@@ -7,6 +7,9 @@
  **/
 class AjaxController extends Epic_Controller_Action
 {
+  public function compareAction() {
+    var_dump($this->getRequest()->getParams()); exit;
+  }
   public function buildsAction() {
     $username = $this->getRequest()->getParam("username");
     if(!$username) {
