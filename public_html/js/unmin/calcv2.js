d6f0967e8dcb92fad1a5e5204c3aaf5ed5a2ed7e
Aaron Cox <aaron.cox@greymass.com>
2012-09-19 13:23:33 -0500

    Fixing a strange situation in sim where removing +ele damage breaks mhRealDamage.

--- public_html/js/unmin/calcv2.js
+++ public_html/js/unmin/calcv2.js
@@ -686,7 +686,7 @@ BuildCalculator.prototype = {
 		// Elemental Damage Bonuses
 		_.each(['plus-fire-damage', 'plus-arcane-damage', 'plus-poison-damage', 'plus-cold-damage', 'plus-lightning-damage', 'plus-holy-damage'], function(v,k) {
 			if(_.has(this.attrs, v)) {
-				// console.log(this.attrs[v]);
+        console.log(this.attrs[v]);
 				bnElePercent += this.attrs[v];
 			}
 		}, this);
@@ -698,7 +698,7 @@ BuildCalculator.prototype = {
 				bnEleDamage += (this.attrs.mhRealDamage.min + bnMinDamage) * (bnElePercent / 100);
 			}
 		}
-		// console.log(this.attrs.mhRealDamage.min, bnMinDamage, bnElePercent, bnEleDamage);
+    console.log(this.attrs.mhRealDamage.min, bnMinDamage, bnElePercent, bnEleDamage);
 		// Are we duel wielding?
     // console.log(this.attrs);		
 		var mathS, mathC, mathR, mathA, mathM;
@@ -1444,7 +1444,7 @@ BuildCalculator.prototype = {
 					case "cold-damage":
 					case "holy-damage":
 						this.attrs[ak] = av;
-						if(this.attrs['damage']) {
+						if(this.attrs['damage'] && typeof(av) == 'object') {
 							if(slot == "mainhand") {
 								this.attrs['mhRealDamage'] = {
 									min: this.attrs['damage']['min'] - av.min,
