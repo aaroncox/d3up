03f0e594833bc100a63b47b27f886a55933d6b59
Aaron Cox <aaron.cox@greymass.com>
2012-09-26 17:54:56 -0500

    Force it to crawl as that user.

--- application/controllers/RecordController.php
+++ application/controllers/RecordController.php
@@ -338,7 +338,7 @@ class RecordController extends D3Up_Controller_Action
 		// Admin Diagnostics
 		if($profile && $profile->id == 2) {
 		  echo "<pre>"; var_dump($record->export(), $profile->export()); 
-  		D3Up_Tool_Crawler::getInstance()->crawl($record, $profile, $record->_characterId);		  
+  		D3Up_Tool_Crawler::getInstance()->crawl($record, $record->_createdBy, $record->_characterId);		  
 		  $record->crawlCount++;
   		$record->_lastCrawl = time();
   		$record->save();
