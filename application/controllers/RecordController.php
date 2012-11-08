475c6aa43c263ba3f03e6bca79e04fc1e6dc00e5
Aaron Cox <aaron.cox@greymass.com>
2012-11-08 16:39:58 -0600

    Changed crawl time to 2 minutes.

--- application/controllers/RecordController.php
+++ application/controllers/RecordController.php
@@ -368,8 +368,8 @@ class RecordController extends D3Up_Controller_Action
 			throw new Exception("[RG] This build isn't attached to a Battle.net Profile");
 		}
 		if($record->_createdBy && $profile && $profile->id == $record->_createdBy->id) {
-  		if(time() <= $record->_lastCrawl + 60 * 15) {
-        throw new Exception("Profiles may only be crawled once every 15 minutes. This profile has been updated too recently to be updated again, try again later!");
+  		if(time() <= $record->_lastCrawl + 60 * 2) {
+        throw new Exception("Profiles may only be crawled once every 2 minutes. This profile has been updated too recently to be updated again, try again later!");
   		}
   		if(!$record->_characterRg || !$record->_characterBt || !$record->_characterId) {
     		$this->_redirect("/b/" . $record->id ."/crawl");
