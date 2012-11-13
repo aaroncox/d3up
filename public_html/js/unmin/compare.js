69cc8dad361f464d826d0a2273a19d05e64e23a6
Aaron Cox <aaron.cox@greymass.com>
2012-11-13 16:10:59 -0600

    Added percent to compare.

--- public_html/js/unmin/compare.js
+++ public_html/js/unmin/compare.js
@@ -11,7 +11,7 @@
 			    compare = d3up.builds[target],
           tBody = $("<tbody/>"),
           tHeader = $("<tr/>");
-      tHeader.append("<th>Stat</th>", "<th class='diff'>Diff</th>", "<th>Old</th>", "<th>New</th>");
+      tHeader.append("<th>Stat</th>", "<th class='diff'>Diff</th>", "<th>%</th>", "<th>Old</th>", "<th>New</th>");
       tBody.append(tHeader);
       $.each(diff, function(k,v) {
   			var data = v.split("|"),
@@ -20,7 +20,8 @@
   					row = $("<tr/>");
   			row.append($("<td/>").html(diffName));
   			var oldStat = Math.round(build.stats[k] * 10) / 10,
-  					newStat = Math.round(compare.stats[k] * 10) / 10;				
+  					newStat = Math.round(compare.stats[k] * 10) / 10,
+						change = Math.round(100 - (newStat / oldStat * 100)) * -1;				
   			if(oldStat > 9999) {
   				oldStat = Math.round(oldStat / 100) / 10 + "k";
   			}
@@ -32,10 +33,12 @@
 				newStat = newStat.toString().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,");			
   			if(data[1] > 0) {
   				row.append($("<td class='diff'>").html("+"+diffVal).addClass("pos"));
+          row.append($("<td class='per pos'/>").html(change + "%"));       
   				row.append($("<td class='neg'>").html(oldStat));
   				row.append($("<td class='pos'>").html(newStat));				
   			} else {
   				row.append($("<td class='diff'>").html(diffVal).addClass("neg"));
+          row.append($("<td class='per neg'/>").html(change + "%"));       
   				row.append($("<td class='pos'>").html(oldStat));
   				row.append($("<td class='neg'>").html(newStat));				
   			}
