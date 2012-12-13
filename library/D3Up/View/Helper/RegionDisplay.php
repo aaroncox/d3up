aa63b3021a4cc63833c5975a3b8d65256361c8f4
Aaron Cox <aaron.cox@greymass.com>
2012-12-13 17:41:50 -0600

    More compact display of tooltip.

--- library/D3Up/View/Helper/RegionDisplay.php
+++ library/D3Up/View/Helper/RegionDisplay.php
@@ -12,8 +12,21 @@ class D3Up_View_Helper_RegionDisplay extends Zend_View_Helper_Abstract
 		2 => 'Europe',
 		3 => 'Asia',
 	);
+
+	protected $_short = array(
+		1 => 'US',
+		2 => 'EU',
+		3 => 'AS',
+	);
 	
-	public function regionDisplay($id) {
+	public function regionDisplay($id, $format = null) {
+		if($format) {
+			switch($format) {
+				case "short":
+					return $this->_short[$id];
+					break;
+			}
+		}
 		return $this->_regions[$id];
 	}
 } // END class D3Up_View_Helper_MethodCleaner extends Zend_View_Helper_Abstract
