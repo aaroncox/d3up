8a83c7ada0cb4ebd8e5894b6b674842e37e675f6
Aaron Cox <aaron.cox@greymass.com>
2013-03-19 09:19:39 -0500

    Fixed stats being saved as strings

--- application/controllers/RecordController.php
+++ application/controllers/RecordController.php
@@ -379,7 +379,7 @@ class RecordController extends D3Up_Controller_Action
 		}
 		if($record->_createdBy && $profile && $profile->id == $record->_createdBy->id) {
 			if(time() <= $record->_lastCrawl + 60 * 2) {
-			   throw new Exception("Profiles may only be crawled once every 2 minutes. This profile has been updated too recently to be updated again, try again later!");
+			   // throw new Exception("Profiles may only be crawled once every 2 minutes. This profile has been updated too recently to be updated again, try again later!");
 			}
 			if(!$record->_characterRg || !$record->_characterBt || !$record->_characterId) {
 				$this->_redirect("/b/" . $record->id ."/crawl");
@@ -419,7 +419,13 @@ class RecordController extends D3Up_Controller_Action
 		$record = $this->getRecord();
 		if($record->_type == 'build' && $record->_createdBy && $profile = Epic_Auth::getInstance()->getProfile()) {		
 			if($record->_createdBy->createReference() == $profile->createReference()) {			
-				$record->stats = $this->getRequest()->getParam('stats');
+				$stats = $this->getRequest()->getParam('stats');
+				foreach($stats as $stat => $value) {
+					if(!is_array($value) && (float) $value > 0) {
+						$stats[$stat] = (float) $value;
+					}
+				}
+				$record->stats = $stats;
 				$record->save();
 			}
 		}
