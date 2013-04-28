d77e7e0b8cab44356682508e87ccbe1768044129
Aaron Cox <aaron.cox@greymass.com>
2013-04-28 17:44:23 -0500

    Adds cookie if user is logged in.

--- library/D3Up/Controller/Action.php
+++ library/D3Up/Controller/Action.php
@@ -8,6 +8,9 @@
 class D3Up_Controller_Action extends Epic_Controller_Action
 {
 	public function preDispatch() {
+		// $this->getResponse()->setHeader('Expires', '', true);
+		// $this->getResponse()->setHeader('Cache-Control', 'private', true);
+		// $this->getResponse()->setHeader('Pragma', '', true);
 	  $this->view->featuredGuide = array('$in' => array(
 	   88, // General
 	   30, // Monk
@@ -19,11 +22,17 @@ class D3Up_Controller_Action extends Epic_Controller_Action
 		$this->view->profile = $profile = D3Up_Auth::getInstance()->getProfile();
 		$this->view->navRecentNews = Epic_Mongo::db('update')->fetchAll(array(), array("_created" => -1), 5);
 		if(!$profile) {
+			if(isset($_COOKIE['d3up_user']))  {
+				setcookie("d3up_user", false, time() - (10 * 365 * 24 * 60 * 60), "/");				
+			}
 			$loginForm = $this->view->loginForm = new Epic_Auth_Form_Login();
 			$loginForm->username->setLabel(null)->setAttrib('placeholder', 'Username');
 			$loginForm->password->setLabel(null)->setAttrib('placeholder', 'Password');
-			$this->_handleForm($loginForm);
+			$loginForm->setAction("/user/login");
 		} else {
+			if(!isset($_COOKIE['d3up_user'])) {
+				setcookie("d3up_user", true, time() + (10 * 365 * 24 * 60 * 60), "/");
+			}
 			$query = array('_createdBy' => $profile->createReference());
 			$sort = array("paragon" => -1, "level" => -1);
 			$this->view->userBuilds = Epic_Mongo::db('build')->fetchAll($query, $sort, 5);
